e1673ac4d15842729a50dff918f81573d772f0a8
chris-young94 <yanghaihang2014@outlook.com>
2019-03-12 14:26:30 +0800

    for 双变量

--- src/main/java/com/first/test/demo/demo2/Result.java
+++ src/main/java/com/first/test/demo/demo2/Result.java
@@ -1,13 +1,8 @@
 package com.first.test.demo.demo2;
 
-import com.first.test.demo.demo.Robot;
-
 import java.io.IOException;
 import java.math.BigInteger;
-import java.text.DecimalFormat;
-import java.util.Date;
 
-import static com.sun.tools.javac.util.Constants.format;
 
 public class Result implements Runnable {
 
@@ -23,35 +18,16 @@ public class Result implements Runnable {
 
     public static void main(String[] args) {
 
-
-        Result result = new Result(1, 500);
-        new Thread(result).start();
-        Result result1 = new Result(501, 1000);
-        new Thread(result1).start();
-        Result result2 = new Result(1001, 1500);
-        new Thread(result2).start();
-        Result result3 = new Result(1501, 2000);
-        new Thread(result3).start();
-        Result result4 = new Result(2001, 2500);
-        new Thread(result4).start();
-        Result result5 = new Result(2501, 3000);
-        new Thread(result5).start();
-        Result result6 = new Result(3001, 3500);
-        new Thread(result6).start();
-        Result result7 = new Result(3501, 4000);
-        new Thread(result7).start();
-        Result result8 = new Result(4001, 4500);
-        new Thread(result8).start();
-        Result result9 = new Result(4501, 5000);
-        new Thread(result9).start();
         Result result10 = new Result(5001, 5105);
         new Thread(result10).start();
 
+        for(int c= 1,b=500;c<=5000;c=c+500,b=b+500){
+            new Thread(new Result(c,b)).start();
+        }
     }
 
     @Override
     public void run() {
-
 
         while (i <= y) {
             BigInteger I = BigInteger.valueOf(i);
@@ -72,7 +48,6 @@ public class Result implements Runnable {
                     }
                     Counter.add(value3);
                     System.out.println("Counter.counter=" + Counter.counter);
-
                 }
             } catch (IOException e) {
                 e.printStackTrace();
